5d4d33672756c79b5dde2d919b605415b5f9e345
Tomás Menna <pablotomasmenna@gmail.com>
2023-07-01 01:24:52 -0300

    ApiRest y imagenes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const linkElement1 = document.getElementById('git1');
+  const linkElement2 = document.getElementById('git2');
+  const imgPerfil = document.getElementById('ImgPerfil');
+  const NomPerfil = document.getElementById('n-perfil');
 
-fetch("https://api.github.com/repos/Another818/AsistenteVirtual")
-  .then(res => res.json())
-  .then(response => {
-  console.log(response)
-})
+  fetch("https://api.github.com/repos/Another818/AsistenteVirtual")
+    .then(res => res.json())
+    .then(response => {
+      linkElement1.href = response.html_url;
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+
+    fetch("https://api.github.com/repos/Another818/Proyecto_6")
+    .then(res => res.json())
+    .then(response => {
+      linkElement2.href = response.html_url;
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+
+    fetch("https://api.github.com/users/Another818")
+    .then(res => res.json())
+    .then(response => {
+      imgPerfil.src = response.avatar_url;
+      NomPerfil.textContent = response.name
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+});
--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,13 @@ const menuNav = document.querySelector('.menu-navegacion')
 
 menu.addEventListener('click', ()=>{
     menuNav.classList.toggle("spread")
-
+    menu.style.visibility = "hidden";
 })
 
 window.addEventListener('click', e=>{
     if(menuNav.classList.contains('spread') && e.target != menuNav && e.target != menu  ){
     
         menuNav.classList.toggle("spread")
+        menu.style.visibility = "visible";
     }
 })
