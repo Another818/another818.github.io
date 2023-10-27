2cb423ac0a18abf18eb875c3ea07d5f9f6b5d113
Tomás Menna <pablotomasmenna@gmail.com>
2023-10-26 22:42:42 -0300

    Fixes and Additions

--- js/aniScroll.js
+++ js/aniScroll.js
@@ -1,20 +1,19 @@
 // Función para verificar si el elemento está visible en el viewport
 function isElementInViewport(element) {
-    var rect = element.getBoundingClientRect();
-    return rect.top <= 0;
-    
-}
-  // Función para manejar el evento de scroll
-function handleScroll() {
-    var element = document.querySelector('.cont-sobremi');
-    var element2 = document.querySelector('.cont-perfil')
-    
-    if (isElementInViewport(element)) {
-        element2.classList.add('animacion-perfil');
-    } else {
-        element2.classList.remove('animacion-perfil');
-    }
+  var rect = element.getBoundingClientRect();
+  return rect.top <= 0;
 }
 
-  // Escuchar el evento de scroll
+// Función para manejar el evento de scroll
+function handleScroll() {
+  var element = document.querySelector('.cont-sobremi');
+  var element2 = document.querySelector('.cont-perfil');
+  
+  if (isElementInViewport(element) && !element2.classList.contains('no-animar')) {
+      element2.classList.add('animacion-perfil');
+      element2.classList.add('no-animar'); // Agregar la clase de control
+  }
+}
+
+// Escuchar el evento de scroll
 window.addEventListener('scroll', handleScroll);
--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
   const linkElement1 = document.getElementById('git1');
   const linkElement2 = document.getElementById('git2');
+  const linkElement3 = document.getElementById('git3');
+  const linkElement4 = document.getElementById('git4');
   const imgPerfil = document.getElementById('ImgPerfil');
   const NomPerfil = document.getElementById('n-perfil');
 
@@ -17,6 +19,24 @@ document.addEventListener('DOMContentLoaded', function() {
     .then(res => res.json())
     .then(response => {
       linkElement2.href = response.html_url;
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+
+    fetch("https://api.github.com/repos/Another818/crud-asp-net")
+    .then(res => res.json())
+    .then(response => {
+      linkElement3.href = response.html_url;
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+
+    fetch("https://api.github.com/repos/Another818/django-crud-react")
+    .then(res => res.json())
+    .then(response => {
+      linkElement4.href = response.html_url;
     })
     .catch(error => {
       console.log('Error:', error);
