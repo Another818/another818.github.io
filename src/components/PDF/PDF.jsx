f294fbc665f99c4be59e055c40340459d6628b3b
Tomás Menna <pablotomasmenna@gmail.com>
2024-01-17 17:39:13 -0300

    Fixed QR img 2

--- src/components/PDF/PDF.jsx
+++ src/components/PDF/PDF.jsx
@@ -216,7 +216,7 @@ function PDF() {
                         <Text style={styles.parragraph_2}>• Inglés: Lee - Escribe (Actualmente realizando cursos)</Text>
                     </View>
                     <View style={styles.qr_box}>
-                        <Image src={require("img/qrcode.svg")} style={styles.image_qr}/>
+                        <Image src="img/qrcode.svg" style={styles.image_qr}/>
                     </View>
                 </Page>
             </Document>
